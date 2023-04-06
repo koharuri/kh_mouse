d0605ce2a99086d2dc4abde7688f32ad85be51bd
koharuri <130030833+koharuri@users.noreply.github.com>
2023-04-06 14:25:54 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1680761969,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"media/決定ボタンを押す4.ogg",
+		"media/決定ボタンを押す4 (online-audio-converter.com).m4a",
+		"media/決定ボタンを押す4 (online-audio-converter.com).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
